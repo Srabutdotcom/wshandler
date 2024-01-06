52729099630c82fcfea6d73bfaec2cc01582df5b
handoko <srabutdotcom@gmail.com>
2024-01-06 18:32:24 +0700

    add bind(this) to handle method

--- wshandler.js
+++ wshandler.js
@@ -3,15 +3,17 @@ import { parseBlob } from "./deps.js";
 
 class HandleWs {
    handler = {};
-   constructor(){ }
+   constructor() {
+      this.handle = this.handle.bind(this);
+   }
 
-   addHandler(action, handler){
-      if(!this.handler?.[action])this.handler[action] = handler;
+   addHandler(action, handler) {
+      if (!this.handler?.[action]) this.handler[action] = handler;
       return
    }
 
-   async handle(data, socket){
-      const {action, data: _data} = await parseBlob(data)
+   async handle(data, socket) {
+      const { action, data: _data } = await parseBlob(data)
       return this.handler?.[action](_data, socket)
    }
 
